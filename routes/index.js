6900c94b43b3ccbfe1608e27bfb310a3a3863463
sapardo10 <sa.pardo10@uniandes.edu.co>
2018-03-09 02:38:02 -0500

    Solucionados errores de backend y fucionalidad del api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  res.send({ title: 'Express' });
+});
+
+router.get('/user/:name', function(req, res, next) {
+  request({
+  url: 'https://www.instagram.com/'+req.params.name+'/?__a=1',
+  json: true
+}, function(error, response, body) {
+  console.log(body);
+  res.send(body['user']['media']['nodes']);
+});
+  //res.send({ title: 'Express', name: req.params.name });
 });
 
 module.exports = router;
